359ca05920e53783c6e6591187945c6a52998d24
Sai krishna <saikrishna321@gmail.com>
2021-03-14 23:46:29 +0530

    Code updated
    Using the well known Yuvraj 6 6's in an over video from ICC main youtube channel.

--- Youtube video downloader/yt_video_downloader.py
+++ Youtube video downloader/yt_video_downloader.py
@@ -2,7 +2,29 @@
 """
 Created on Sun Mar 14 23:36:06 2021
 
-@author: kqll413
+@author: Sai Krishna
 """
 
-import pandas as pd
+#Setting directory for download location
+import os
+os.chdir('C:\\Data\\github\\python_projects\\Youtube video downloader')
+
+#Importing pytube (use pip install pytube)
+from pytube import YouTube
+
+#Video link as a string
+link = "https://www.youtube.com/watch?v=8b0ubLO2MUE"
+
+video = YouTube(link)
+
+#Using the best resolution option
+stream = video.streams.get_highest_resolution()
+
+#File name of the video about to be downloaded
+stream.default_filename
+
+#Title of the video
+stream.title
+
+#Download command
+stream.download()
